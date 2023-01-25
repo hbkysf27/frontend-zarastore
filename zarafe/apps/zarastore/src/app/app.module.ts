8ca4bf3a144b6bdb8044b519cdb2de927bbc7a44
Yusuf <Yusufsalih4@gmail.com>
2023-01-25 13:00:17 +0530

    masterpage with header & footer components

--- zarafe/apps/zarastore/src/app/app.module.ts
+++ zarafe/apps/zarastore/src/app/app.module.ts
@@ -6,16 +6,17 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
-  {path:'', component: HomePageComponent},
-  {path:'products',component:ProductListComponent}
-]
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
+];
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
-    imports: [BrowserModule,RouterModule.forRoot(routes)],
+    declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
     providers: [],
     bootstrap: [AppComponent]
 })
