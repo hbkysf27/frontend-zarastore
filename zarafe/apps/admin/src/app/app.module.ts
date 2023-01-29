faf6d48c0b0e5bd8dcf4657b1b7163da2f203aa1
Yusuf <Yusufsalih4@gmail.com>
2023-01-29 11:22:58 +0530

    products form component

--- zarafe/apps/admin/src/app/app.module.ts
+++ zarafe/apps/admin/src/app/app.module.ts
@@ -26,6 +26,11 @@ import {ToastModule} from 'primeng/toast';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import {ColorPickerModule} from 'primeng/colorpicker';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {DropdownModule} from 'primeng/dropdown';
+import {EditorModule} from 'primeng/editor';
 
 
 const UX_MODULE=[
@@ -36,7 +41,12 @@ const UX_MODULE=[
     InputTextModule,
     ToastModule,
     ConfirmDialogModule,
-    ColorPickerModule
+    ColorPickerModule,
+    InputNumberModule,
+    InputTextareaModule,
+    InputSwitchModule,
+    DropdownModule,
+    EditorModule,
 
 
 ]
--- zarafe/apps/admin/src/app/pages/products/products-form/products-form.component.ts
+++ zarafe/apps/admin/src/app/pages/products/products-form/products-form.component.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoriesService } from '@zarafe/products';
+
 
 @Component({
-  selector: 'zarafe-products-form',
+  selector: 'admin-products-form',
   templateUrl: './products-form.component.html',
   styles: [
   ]
 })
 export class ProductsFormComponent {
+
+  editmode=false;
+  form: FormGroup;
+  isSubmitted=false;
+  categories=[];
+
+  constructor(private formBuilder: FormBuilder, private categoriesService: CategoriesService){}
+
+  ngOnInit(): void{
+    this._initForm();
+    this._getCategories();
+  }
+
+  private _initForm(){
+    this.form=this.formBuilder.group({
+      name: ['', Validators.required],
+      brand: ['', Validators.required],
+      price: ['', Validators.required],
+      category: ['', Validators.required],
+      countInStock: ['', Validators.required],
+      description: ['', Validators.required],
+      richDescription: [''],
+      image: ['', Validators.required],
+      isFeatured: [false],
+
+    });
+  }
+
+  private _getCategories() {
+    this.categoriesService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+  }
+  get productForm() {
+    return this.form.controls;
+  }
+
+  onSubmit(){
+
+  }
+  onCancle(){
+
+  }
 
 }
